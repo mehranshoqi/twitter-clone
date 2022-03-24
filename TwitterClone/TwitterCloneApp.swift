71e0f12fc4d5a69bf5b8fe2584026abe57e883f3
mehranshoqi <mehranshoqi@outlook.com>
2022-03-24 17:28:37 +0430

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by mehran on 2022-03-24.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
